30cf0afa78a725be78253a840d497075c4a4f160
Pablo Arenas <pablo.arenas.rodriguez.alu@iesjulioverne.es>
2024-01-29 10:33:32 +0100

    Primer Commit Componente

---
+++ src/app/components/pablo/pablo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pablo',
+  templateUrl: './pablo.component.html',
+  styleUrls: ['./pablo.component.scss'],
+})
+export class PabloComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
