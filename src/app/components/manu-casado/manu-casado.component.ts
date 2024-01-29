d99b62e482c4b4cac7e12cd94187cdd21a392972
ManuCG_Clase <manueljesus.casado.gomez.alu@iesjulioverne.es>
2024-01-29 10:33:30 +0100

    Mi componente

---
+++ src/app/components/manu-casado/manu-casado.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manu-casado',
+  templateUrl: './manu-casado.component.html',
+  styleUrls: ['./manu-casado.component.scss'],
+})
+export class ManuCasadoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
