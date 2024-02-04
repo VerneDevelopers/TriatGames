496a6dbae88a1b214d0b3da50e5ba39c2ec6febb
bernat <bernat13@gmail.com>
2024-02-04 19:46:08 +0100

    cambios en el spec.ts para pasar los test

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import {
+  Auth,
+  AuthModule
+} from '@angular/fire/auth';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [  AuthModule,
+        { provide: Auth, useValue: {
+          currentUser: { uid: 'test-uid' },
+        
+        } }
+      ]
+      
+    });
     service = TestBed.inject(AuthService);
   });
 
