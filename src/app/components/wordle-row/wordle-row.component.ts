aa15648b6df8ceaa4f5c6c8eda119e624fd1cf30
ManuCG_Clase <manueljesus.casado.gomez.alu@iesjulioverne.es>
2024-02-05 11:15:30 +0100

    no funcionan las clases

--- src/app/components/wordle-row/wordle-row.component.ts
+++ src/app/components/wordle-row/wordle-row.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'wordle-row',
@@ -6,9 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wordle-row.component.scss'],
 })
 export class WordleRowComponent  implements OnInit {
+  @Input() palabra : string = '';
+  @Input() aciertos : string = '';
+  letras: string[] = ['','','','','']
+  colores : string[] = ['V','A','G','G','G']
+  constructor() {
+    
+   }
 
-  constructor() { }
+  ngOnInit() {
+    console.log("Palabra:", this.palabra)
+    this.palabra= this.palabra.toUpperCase()
+    if (this.palabra.length != 0){
+      this.letras= this.palabra.split('');
 
-  ngOnInit() {}
+    }
+  }
 
 }
