a1db7cafdcf2bf84751611342043a431af4fcc5e
aaronmorenobulnes <aaron.moreno.bulnes.alu@iesjulioverne.es>
2024-02-07 12:34:22 +0100

    comienzoServicio

---
+++ src/app/services/ahorcado/ahorcado.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Firestore, collection, doc, setDoc, deleteDoc, where, query, getDocs } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Data } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AhorcadoService {
+  constructor(private afs: AngularFirestore, private firestore: Firestore) { }
+
+  addJugada(userId:string, letra:string, fecha:Data) {
+    const documentRef = doc(collection(this.firestore, `users/${userId}/Ahorcado`));
+
+    const data = {
+        letra: letra,
+        fecha: fecha
+    };
+
+    return setDoc(documentRef, data);
+}
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,24 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {}
+  firebaseConfig : {
+
+    apiKey: "AIzaSyC5cy7dhKPD27MUyHQ8q_qx_F19BRCmkT0",
+  
+    authDomain: "ahorcado-d7630.firebaseapp.com",
+  
+    projectId: "ahorcado-d7630",
+  
+    storageBucket: "ahorcado-d7630.appspot.com",
+  
+    messagingSenderId: "1083726545017",
+  
+    appId: "1:1083726545017:web:ca2ef9d1d45ccd355894a8",
+  
+    measurementId: "G-7FHJ7MJ29N"
+  
+  }
+  
 };
 
 /*
