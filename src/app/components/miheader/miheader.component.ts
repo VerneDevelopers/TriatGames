414ac17ae01675c5fbe51a7b9ed7f2c34c3ed6a7
ManuCG_Clase <manueljesus.casado.gomez.alu@iesjulioverne.es>
2024-02-07 12:49:26 +0100

    implementacion avatar

--- src/app/components/miheader/miheader.component.ts
+++ src/app/components/miheader/miheader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { sha256 } from 'js-sha256';
 
 @Component({
   selector: 'app-miheader',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiheaderComponent  implements OnInit {
 
-  usuario : string = "Alex@gmail.com"
+  usuario : string = "manueljesus.casado.gomez.alu@iesjulioverne.es"
+
+  getGravatarURL(email: string): string {
+    // Trim leading and trailing whitespace from
+    // an email address and force all characters
+    // to lower case
+    const address = email.trim().toLowerCase();
+
+    // Create a SHA256 hash of the final string
+    const hash = sha256(address);
+
+    // Grab the actual image URL
+    return `https://www.gravatar.com/avatar/${hash}`;
+  }
   
   constructor() { 
     
