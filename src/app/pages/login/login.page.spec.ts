071f74f70d160a25396f719176f1faf6c8cfb723
Julian <julian.garrido.quintero.alu@iesjulioverne.es>
2024-02-08 10:02:39 +0100

    cambios tests

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,11 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPage } from './login.page';
+import { AuthService } from 'src/app/services/auth.service';
+import { Auth } from '@angular/fire/auth';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
-
+  let service: AuthService;
+  
   beforeEach((() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide:
+            Auth,
+          useValue: {
+            currentUser: {
+              uid: 'test-uid'
+            }
+          },
+       
+        }
+      ]
+    });
+    service = TestBed.inject(AuthService);
+  
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/pages/login/registro/registro.page.spec.ts
+++ src/app/pages/login/registro/registro.page.spec.ts
@@ -1,11 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegistroPage } from './registro.page';
+import { Auth } from '@angular/fire/auth';
+import { AuthService } from 'src/app/services/auth.service';
 
 describe('RegistroPage', () => {
   let component: RegistroPage;
   let fixture: ComponentFixture<RegistroPage>;
+  let service: AuthService;
 
   beforeEach((() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide:
+            Auth,
+          useValue: {
+            currentUser: {
+              uid: 'test-uid'
+            }
+          },
+       
+        }
+      ]
+    });
+    service = TestBed.inject(AuthService);
     fixture = TestBed.createComponent(RegistroPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
