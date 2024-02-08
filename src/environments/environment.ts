b3acf3b8ba15712f0b8f5429eb592bf4531aa0d5
Julian <julian.garrido.quintero.alu@iesjulioverne.es>
2024-02-08 09:49:06 +0100

    Sin enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBFsS0oOKjnjazHg7Ka9PAqKUf9A3_t2hc",
-    authDomain: "pruebatriatgames.firebaseapp.com",
-    projectId: "pruebatriatgames",
-    storageBucket: "pruebatriatgames.appspot.com",
-    messagingSenderId: "957859222692",
-    appId: "1:957859222692:web:da8f896691105aaef9c231"
+    
   }
 };
 
