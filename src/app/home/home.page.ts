aaf29e85d30ff8ae8014a9b50c14d2009f9cc108
parerod <pablo.arenas.rodriguez.alu@iesjulioverne.es>
2024-02-11 15:04:17 +0100

    Ahora si, Servicio terminado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,47 +8,9 @@ import { ServicioWordleService } from '../services/servicio-wordle.service';
 })
 export class HomePage {
 
-  constructor(private servi : ServicioWordleService) {}
+  constructor() {}
 
-  ngOnInit() {
-    
-  }
-
-  fire() {
-    var idUser = "pepe"
-    var palabra = "arcos"
-    var fecha = "04-02-2024"
-    
-    this.servi.addJugada(idUser, palabra, fecha)
-  }
-
-  fecha() {
-    const opcionesDeFormato: Intl.DateTimeFormatOptions = {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit"
-    };
-    var fecha = new Date();
-
-    var fechaString : string = fecha.toLocaleString("es-ES", opcionesDeFormato);
-
-    console.log(fechaString)
-  }
-
-  palabra() {
-    var palabra = this.servi.palabraDia()
-    console.log(palabra)
-  }
-
-  verPalabras() {
-    this.servi.misJugadas("pepe","04-02-2024").subscribe(
-      resp => {
-        for (var i = 0; i < resp.length; i++) {
-          console.log(resp[i]);
-        }
-      }
-    )
-  }
+  
 
     
 
