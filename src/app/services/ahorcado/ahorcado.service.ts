b551635ae4da02b052b89ca31a236a86e2d72da1
aaronmorenobulnes <aaron.moreno.bulnes.alu@iesjulioverne.es>
2024-02-11 17:14:41 +0100

    Final??

--- src/app/services/ahorcado/ahorcado.service.ts
+++ src/app/services/ahorcado/ahorcado.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, doc, setDoc, deleteDoc, where, query, getDocs } from '@angular/fire/firestore';
+import { Firestore, collection, doc, setDoc, getDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Data } from '@angular/router';
@@ -10,22 +10,31 @@ import { Data } from '@angular/router';
 export class AhorcadoService {
   constructor(private afs: AngularFirestore, private firestore: Firestore) { }
 
-  addJugada(userId: string, letra: string, fecha: Date) {
+  async addJugada(userId: string, letra: string, fecha: Date): Promise<boolean> {
     const opcionesDeFormato: Intl.DateTimeFormatOptions = {
       year: "numeric",
       month: "2-digit",
       day: "2-digit"
     };
 
+    const data = { letra };
+
     const fechaFormato: string = fecha.toLocaleString("es-ES", opcionesDeFormato)
       .split('/').reverse().join('/');
     const documentRef = doc(collection(this.firestore, `${userId}/Ahorcado/${fechaFormato}`));
 
-    const data = {
-      letra
-    };
+    //Comprobacion de que no este ya la letra, que no se ha jugado ya la letra que se pasa por parametro
+    const docSnapshot = await getDoc(documentRef);
+    if (docSnapshot.exists()) {
+      const data2 = docSnapshot.data();
+      if (data2 && data2 === data) {
+        return false;
+      }
+    }
 
-    return setDoc(documentRef, data);
+  
+    await setDoc(documentRef, data);
+    return true;
   }
 
   getLetraporDia(userId: string, fecha: Date): Observable<Data[]> {
