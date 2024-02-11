ae4d10142422a372fe3e19563fca4f2d2c27ecb4
aaronmorenobulnes <aaron.moreno.bulnes.alu@iesjulioverne.es>
2024-02-11 11:37:31 +0100

    CasiAcabado

--- src/app/pages/ahorcado/ahorcado.page.ts
+++ src/app/pages/ahorcado/ahorcado.page.ts
@@ -13,7 +13,12 @@ export class AhorcadoPage implements OnInit {
 
   ngOnInit() {
     var fecha:Date=new Date()
-    this.miServicio.addJugada("userid","a",fecha)
+    this.miServicio.getLetraporDia("userid",fecha).subscribe(
+      (s) => {
+        console.log(s)
+      },
+      (error) => console.error(error)
+    );
   }
 
 }
--- src/app/services/ahorcado/ahorcado.service.ts
+++ src/app/services/ahorcado/ahorcado.service.ts
@@ -10,21 +10,34 @@ import { Data } from '@angular/router';
 export class AhorcadoService {
   constructor(private afs: AngularFirestore, private firestore: Firestore) { }
 
-  addJugada(userId:string, letra:string, fecha:Date) {
+  addJugada(userId: string, letra: string, fecha: Date) {
     const opcionesDeFormato: Intl.DateTimeFormatOptions = {
       year: "numeric",
       month: "2-digit",
       day: "2-digit"
-  };
-  
-  const fechaFormato: string = fecha.toLocaleString("en-EN", opcionesDeFormato);
-    const documentRef = doc(collection(this.firestore, `${userId}/Ahorcado/${fechaFormato}/`));
+    };
+
+    const fechaFormato: string = fecha.toLocaleString("es-ES", opcionesDeFormato)
+      .split('/').reverse().join('/');
+    const documentRef = doc(collection(this.firestore, `${userId}/Ahorcado/${fechaFormato}`));
 
     const data = {
-        letra
+      letra
     };
 
     return setDoc(documentRef, data);
-}
+  }
+
+  getLetraporDia(userId: string, fecha: Date): Observable<Data[]> {
+    const opcionesDeFormato: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit"
+    };
+
+    const fechaFormato: string = fecha.toLocaleString("es-ES", opcionesDeFormato)
+      .split('/').reverse().join('/');
+    return this.afs.collection<Data>(`${userId}/Ahorcado/${fechaFormato}`).valueChanges();
+  }
 
 }
