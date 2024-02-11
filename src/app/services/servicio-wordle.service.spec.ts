80667e58698a45556862142df9cd0345e3fc9057
bernat <bernat13@gmail.com>
2024-02-11 17:22:45 +0100

    pasar test

--- src/app/services/servicio-wordle.service.spec.ts
+++ src/app/services/servicio-wordle.service.spec.ts
@@ -1,12 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ServicioWordleService } from './servicio-wordle.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 describe('ServicioWordleService', () => {
   let service: ServicioWordleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: AngularFirestore,
+          useValue: {
+            collection: () => {
+              return {
+                valueChanges: () => {
+                  return {
+                    subscribe: () => {
+                      return {
+                        unsubscribe: () => {
+                          return;
+                        }
+                      };
+                    }
+                  };
+                }
+              };
+            }
+          }
+        }
+      ]
+    });
     service = TestBed.inject(ServicioWordleService);
   });
 
