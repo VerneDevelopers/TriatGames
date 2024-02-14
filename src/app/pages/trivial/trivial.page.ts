3d93832df94fd1d2822c1f6cb5627f46a9b101ab
Juliang325 <julian.garrido.quintero.alu@iesjulioverne.es>
2024-02-14 18:31:02 +0100

    ya se ve la pregunta

--- src/app/pages/trivial/trivial.page.ts
+++ src/app/pages/trivial/trivial.page.ts
@@ -9,16 +9,39 @@ import { TrivialService } from 'src/app/services/trivial.service';
 })
 export class TrivialPage implements OnInit {
 
-  preguntas:PreguntaTrivial[] = [];
+  preguntas:PreguntaTrivial[];
   pregunta:PreguntaTrivial= {} as PreguntaTrivial ;
   respuestas:string[] = ['respuesta1', 'respuesta2', 'respuesta3', 'respuesta4'];
   numeroIntentos:number=0;
 
   constructor(
     private trivialSvc: TrivialService, 
-    ) { }
+    ) {
+      this.preguntas = [] ; 
+     }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.obtenerPreguntas();
+    console.log('LAS PREGUNTAS SON: ' + this.preguntas);
+  }
+
+  obtenerPreguntas(){
+    this.trivialSvc.getPreguntas().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.preguntas = res;
+        this.pregunta = this.preguntas[1]
+        console.log(this.pregunta);
+      },
+      error: (error:any) =>{
+
+
+      }
+
+    })
   }
 
   responder(r:string){
