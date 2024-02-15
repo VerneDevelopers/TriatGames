f97a64a26ffd8b20aea1c14d8ecc23d562ee81c0
DaniiLlamas <daniel.llamas.patxot.alu@iesjulioverne.es>
2024-02-15 10:14:08 +0100

    mi rama

--- src/app/pages/ahorcado/ahorcado.page.ts
+++ src/app/pages/ahorcado/ahorcado.page.ts
@@ -1,24 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
 import { AhorcadoService } from 'src/app/services/ahorcado/ahorcado.service';
 
+
 @Component({
-  selector: 'app-ahorcado',
-  templateUrl: './ahorcado.page.html',
-  styleUrls: ['./ahorcado.page.scss'],
+ selector: 'app-ahorcado',
+ templateUrl: './ahorcado.page.html',
+ styleUrls: ['./ahorcado.page.scss'],
 })
 export class AhorcadoPage implements OnInit {
+ userUid : string
+ fecha !: Date
+ letra : string = ''
+ constructor(private serv : AhorcadoService, private auth : Auth) {
+   if(this.auth.currentUser?.uid != null)
+   this.userUid = this.auth.currentUser?.uid.toString()
+   else
+   this.userUid = 'Error'
 
-  constructor(private miServicio:AhorcadoService) {
-  }
 
-  ngOnInit() {
-    var fecha:Date=new Date()
-    this.miServicio.getLetraporDia("userid",fecha).subscribe(
-      (s) => {
-        console.log(s)
-      },
-      (error) => console.error(error)
-    );
-  }
+   this.fecha = new Date()
+ }
+
+
+ ngOnInit() {
+   /*
+   var fecha:Date=new Date()
+   this.miServicio.getLetraporDia("userid",fecha).subscribe(
+     (s) => {
+       console.log(s)
+     },
+     (error) => console.error(error)
+   );
+   */
+
+
+ }
+
+
+
+
+ addLetra(){
+   //this.serv.addJugada(this.userUid,this.letra,this.fecha)
+   this.letra = ''
+ }
+
 
 }
+
+
+
+
+
