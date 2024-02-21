df93407cfd2199280af94055881e084f14bcd58c
Alejandro <alexnrdriguez@gmail.com>
2024-02-22 00:47:06 +0100

    CCasaCambioPassOkBorradoCuentaOkCambioNombConsultar

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,45 +13,44 @@ import {
 })
 export class AuthService {
   constructor(private auth: Auth) {
-   
-   }
+
+  }
 
   getUid() {
-    try   {
+    try {
       return this.auth.currentUser?.uid;
     }
     catch (e) {
-    //  console.log(e);
+      //  console.log(e);
       return null;
     }
   }
 
-  async register(email: string, password: string):Promise<UserCredential | null>{
+  async register(email: string, password: string): Promise<UserCredential | null> {
     try {
       const user = await createUserWithEmailAndPassword(this.auth, email, password);
       return user;
     } catch (e) {
-    //  console.log('ha fallado el registro',e);
+      //  console.log('ha fallado el registro',e);
       return null;
     }
   }
 
-  async login( email:string, password :string):Promise<UserCredential | null>  {
+  async login(email: string, password: string): Promise<UserCredential | null> {
     try {
       const user = await signInWithEmailAndPassword(this.auth, email, password);
       return user;
     } catch (e) {
-   //   console.log('ha fallado el login',e);
+      //   console.log('ha fallado el login',e);
       return null;
     }
   }
 
   async resetPassword(email: string) {
     try {
-       await sendPasswordResetEmail(this.auth, email);
+      await sendPasswordResetEmail(this.auth, email);
     } catch (e) {
-    //  console.log(e);
-      
+      //  console.log(e);     
     }
   }
 
@@ -64,4 +63,21 @@ export class AuthService {
       return Promise<void>;
     }
   }
+
+  //Código añadido por Ale para borrado de cuenta
+  eliminarCuenta() {
+    const user = this.auth.currentUser;
+    if (user) {
+      user.delete()
+        .then(() => {
+          console.log('Cuenta eliminada exitosamente.');
+        })
+        .catch((error) => {
+          console.error('Error al eliminar la cuenta:', error.message);
+        });
+    } else {
+      console.error('No hay un usuario autenticado.');
+    }
+  }
 }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,20 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyC2VtO_6L0SbeDRLZninsrLxCQEf15mpcw",
-    authDomain: "triatgames-fee93.firebaseapp.com",
-    projectId: "triatgames-fee93",
-    storageBucket: "triatgames-fee93.appspot.com",
-    messagingSenderId: "23924959621",
-    appId: "1:23924959621:web:cb2e1e30fb3c2972d0bcec",
-    measurementId: "G-NDWVMD99HT"
+    apiKey: "AIzaSyD796HJJAQzOzupK5Q0DJ-4mGFWcOv0bCc",
+
+    authDomain: "pruebastriatgames.firebaseapp.com",
+
+    projectId: "pruebastriatgames",
+
+    storageBucket: "pruebastriatgames.appspot.com",
+
+    messagingSenderId: "74629414060",
+
+    appId: "1:74629414060:web:43d69ea967f4668acab309",
+
+    measurementId: "G-JN88WCT58M"
+
 
   }
 
