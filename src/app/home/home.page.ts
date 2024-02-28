e074e1ab7ab1b88ce268b31566ca15e8d8d790bf
parerod <pablo.arenas.rodriguez.alu@iesjulioverne.es>
2024-02-28 17:37:35 +0100

    Solucionado, pero no se puede probar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ServicioWordleService } from '../services/servicio-wordle.service';
+import { ScoreService } from '../services/score.service';
+import { IPuntuacion } from '../interfaces/i-puntuacion';
+import { Auth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +14,42 @@ export class HomePage {
   /* abrirModal = false;
   resultado = "ganado"; */
 
-  constructor() {}
+  scores! : IPuntuacion[]
+
+  constructor(private servi : ScoreService, private auth : Auth) {}
 
   entradaWordle = false;
   entradaAhorcado= false;
   entradaTrivial = false;
   entradaPuntuaciones = false;
+
+  ngOnInit() {
+    this.repasar()
+  }
   
+
+  repasar() {
+    this.servi.scoreDay(new Date()).subscribe(
+      resp => {
+        this.scores = resp;
+        
+
+        for(var i = 0; i < this.scores.length;i++) {
+          if(this.auth.currentUser?.uid == this.scores[i].idUsuario) {
+            if(this.scores[i].ptsAhorcado == 0) {
+              this.entradaAhorcado  =  true;
+            }
+            if(this.scores[i].ptsTrivial == 0) {
+              this.entradaTrivial  =  true;
+            }
+            if(this.scores[i].ptsWordle == 0) {
+              this.entradaWordle=  true;
+            }
+          }
+        }
+      }
+    )
+  }
 
   /* abrirFinDelJuego() {
     if (!this.abrirModal) {
